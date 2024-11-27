f8a28ee38608d3e2cb46584efbac6f1fc06eef3a
Na0k1i <87078482+Na0k1i@users.noreply.github.com>
2024-11-26 23:11:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Objetivo De Moto',
+    'Moto Objetivo?',
+    'Yamaha R3'
+)
+
+criaCartao(
+    'Namorada',
+    'Meu amor da minha vida é a?',
+    'Julia Franco Guerino (Obvio nekkkk)'
+)
+
+criaCartao(
+    'Rank on Valorant',
+    'Rank?',
+    'Platina Afundado'
+)
+
+criaCartao(
+    'Gamezin',
+    'Qual meu Jogo preferido?',
+    'Valorant'
+)
